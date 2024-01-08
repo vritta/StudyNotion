5357b904217fc22e4407ba9c1f0a7c3b64854e2a
vritta <anuhartripathi15@gmail.com>
2024-01-08 21:35:45 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Home from './pages/Home.js'
-import { NavLink, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Signup from './pages/Signup.js';
+import Dashboard from './pages/Dashboard.js';
+import { Route, Routes } from 'react-router-dom';
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return <div>
-  <Navbar/>
-
-  <NavLink to="/support">Home</NavLink>
+  <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
 
   <Routes>
-    <Route path="/support" element={<Home/>}/>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
   </Routes>
   </div>;
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/Logo.svg';
+import toast from 'react-hot-toast';
 
-
-const Navbar = () => {
+const Navbar = (props) => {
+  const isLoggedIn = props.isLoggedIn;
+  const setIsLoggedIn = props.setIsLoggedIn;
   return (
-    <div>
-      
+    <div className='flex justify-around'>
+      <NavLink to="/">
+        <img src={logo} alt=''/>
+      </NavLink>
+      <nav>
+        <ul className='flex gap-3'>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className='flex ml-3 gap-3'>
+        {
+          isLoggedIn===false?
+          <>
+            <NavLink to="/login">
+              <button>Login</button>
+            </NavLink>
+            <NavLink to="/signup">
+              <button>Sign Up</button>
+            </NavLink>
+          </>:
+          <>
+          <NavLink to="/">
+            <button onClick={()=>{setIsLoggedIn(false); toast.success("Logged Out");}}>
+              Log Out
+            </button>
+          </NavLink>
+          <NavLink to="/dashboard">
+            <button>Dashboard</button>
+          </NavLink>
+          </>
+        }
+      </div>
     </div>
   )
 }
