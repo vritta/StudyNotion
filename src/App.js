ad88a3c08f0c1286a142ee82354670d75bf2b007
vritta <anuhartripathi15@gmail.com>
2024-01-08 15:23:07 +0530

    modification

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from './pages/Home.js'
+import { NavLink, Route, Routes } from 'react-router-dom'
 
 function App() {
   return <div>
   <Navbar/>
+
+  <NavLink to="/support">Home</NavLink>
+
+  <Routes>
+    <Route path="/support" element={<Home/>}/>
+  </Routes>
   </div>;
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Home from '../pages/Home.js'
-import { Link, Route, Routes } from 'react-router-dom'
+
 
 const Navbar = () => {
   return (
     <div>
-      <Routes>
-        <Route to="/" element={<Home/>}/>
-      </Routes>
-      <Link to="/support">Home</Link>
+      
     </div>
   )
 }
