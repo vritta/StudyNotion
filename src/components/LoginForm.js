435a095ad6d4fdfe683da59297c7272fb3440356
vritta <anuhartripathi15@gmail.com>
2024-01-13 16:33:29 +0530

    css modification

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -20,27 +20,39 @@ const LoginForm = ({setIsLoggedIn}) => {
     }
 
   return (
-    <div>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="email"><p>Email Address<sup>*</sup></p></label>
-        <input required type='email' placeholder='Enter email address' 
-        name='email' id='email' value = {formData.email} 
-        onChange={changeHandler}/>
-        <br />
+    <form onSubmit={submitHandler} className='flex flex-col w-full gap-y-4 mt-6'>
+      <label htmlFor="email" className='w-full'>
+        <p className='text-[0.875rem] leading-[1.375rem] mb-1 text-richblack-5'>
+        Email Address<sup className='text-pink-200'>*</sup>
+        </p>
+      </label>
+      <input required type='email' placeholder='Enter email address' 
+      name='email' id='email' value = {formData.email} 
+      onChange={changeHandler} className='bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]'/>
 
-        <label htmlFor="password"><p>Password<sup>*</sup></p></label>
+      <div className='relative'>
+        <label htmlFor="password" className='w-full'>
+          <p className='text-[0.875rem] leading-[1.375rem] mb-1 text-richblack-5'>
+          Password<sup className='text-pink-200'>*</sup>
+          </p>
+        </label>
         <input required type={showPassword?"text":"password"} placeholder='Enter Password' 
         name='password' id='password' value = {formData.password}
-        onChange={changeHandler} 
-        />
+        onChange={changeHandler} className='bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]'/>
 
-        <span onClick={ ()=>setShowPassword((prev)=>!prev) }>
-            {showPassword?<AiOutlineEyeInvisible/>:<AiOutlineEye/>}
+        <span onClick={ ()=>setShowPassword((prev)=>!prev) }
+        className='absolute top-[38px] right-3 cursor-pointer'>
+            {showPassword?<AiOutlineEyeInvisible fontSize={24} fill='#AFB2BF'/>:
+            <AiOutlineEye fontSize={24} fill='#AFB2BF'/>}
         </span>
-        <NavLink to="#"><p>Forgot Password</p></NavLink>
-        <button type='submit'>Sign In</button>
-      </form>
-    </div>
+      </div>
+      <NavLink to="#">
+      <p className='text-xs mt-1 text-blue-100 max-w-max ml-auto'>
+        Forgot Password
+      </p>
+      </NavLink>
+      <button type='submit' className='bg-yellow-50 font-medium rounded-[8px] py-[8px] px-[12px] text-richblack-900 mt-6'>Sign In</button>
+    </form>
   )
 }
 
--- src/components/Template.js
+++ src/components/Template.js
@@ -2,31 +2,40 @@ import React from 'react';
 import frameImage from "../assets/frame.png";
 import SignupForm from './SignupForm';
 import LoginForm from './LoginForm';
+import {FcGoogle} from 'react-icons/fc';
 
 const Template = ({title, desc1, desc2, image, formtype, setIsLoggedIn}) => {
   return (
-    <div>
-      <div>
-        <h1>{title}</h1>
-        <p>
-            <span>{desc1}</span>
-            <span>{desc2}</span>
+    <div className='flex justify-between w-11/12 max-w-[1160px] py-12 mx-auto gap-x-12 gap-y-0'>
+      <div className='w-11/12 max-w-[450px]'>
+        <h1 className='text-richblack-5 font-semibold text-[1.875rem] leading-[2.375rem]'>{title}</h1>
+        
+        <p className='text-[1.25rem] leading[1.625rem] mt-4'>
+            <span className='text-richblack-100'>{desc1}</span>
+            <br />
+            <span className='text-blue-100'>{desc2}</span>
         </p>
         {formtype==="signup"?<SignupForm setIsLoggedIn={setIsLoggedIn}/>
         :<LoginForm setIsLoggedIn={setIsLoggedIn}/>}
 
-        <div>
-          <div></div>
-          <p>OR</p>
-          <div></div>
+        <div className='flex w-full items-center my-4 gap-x-2'>
+          <div className='h-[1px] w-full bg-richblack-700'></div>
+          <p className='text-richblack-700 font-medium leading-[1.375rem]'>OR</p>
+          <div className='h-[1px] w-full bg-richblack-700'></div>
         </div>
-        <button>
+
+        <button className='w-full flex justify-center items-center rounded-[8px] border border-richblack-700 text-richblack-100 font-medium px-[12px] py-[8px] mt-6 gap-x-2'>
+          <FcGoogle/>
           <p>Sign in with Google</p>
         </button>
       </div>
-      <div>
-        <img src={frameImage} alt="" width={558} height={504} loading='lazy'/>
-        <img src={image} alt="" width={558} height={504} loading='lazy'/>
+      
+      <div className='relative w-11/12 max-w-[450px]'>
+        <img src={frameImage} alt="" width={558} height={504} 
+        loading='lazy'/>
+
+        <img src={image} alt="" width={558} height={504} 
+        loading='lazy' className='absolute -top-4 right-4'/>
       </div>
     </div>
   )
